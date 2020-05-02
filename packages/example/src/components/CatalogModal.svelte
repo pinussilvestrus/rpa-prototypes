08bc0578023577b6a44796e166da4e1c47d0524a
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-05-02 17:20:26 +0200

    feat(example): add basic modal

---
+++ packages/example/src/components/CatalogModal.svelte
@@ -0,0 +1,41 @@
+<script>
+    import './CatalogModal.scss';
+
+    const workers = [
+      {
+        idx: 0,
+        name: 'Get Price'
+      },
+      {
+        idx: 1,
+        name: 'Calculate Price'
+      }
+    ];
+
+    export let modalId = '';
+</script>
+  
+<div class="modal" id="{modalId}" aria-hidden="true">
+  <div class="modal-container" role="dialog" aria-modal="true" aria-labelledby="{modalId}-title">
+    <header class="modal-header">
+      <p class="modal-title" id="{modalId}-title">
+        Choose from Worker Catalog
+      </p>
+      <button class="modal-close" aria-label="Close modal" data-micromodal-close></button>
+    </header>
+      <main class="modal-content" id="{modalId}-content">
+        <form>
+          {#each workers as {idx, name}}
+            <div class="worker">
+              <input type="radio" id="{idx}" name="worker" value="{name}">
+              <label for="{idx}">{name}</label><br>
+            </div>
+          {/each}
+        </form>
+    </main>
+    <footer class="modal-footer">
+      <button class="btn btn-primary" data-micromodal-close aria-label="Save Choice">Select</button>
+      <button class="btn btn-secondary" data-micromodal-close aria-label="Close this dialog window">Close</button>
+    </footer>
+  </div>
+</div>
--- packages/example/src/components/Diagram.svelte
+++ packages/example/src/components/Diagram.svelte
@@ -3,11 +3,15 @@
 
   import { forEach } from 'min-dash';
 
-// import dom from 'domtastic';
+  // import dom from 'domtastic';
+
+  import MicroModal from 'micromodal';
 
   import getElement from '../util/getElement';
 
   import DiagramSVG from '../../resources/diagram.svg';
+
+  import CatalogModal from './CatalogModal';
 
   import './Diagram.scss';
 
@@ -16,16 +20,25 @@
     'ServiceTask_2'
   ];
 
+  const modalId = 'catalog-modal';
+
   // lifecycle //////////
 
   onMount(async () => {
 
+    // (1) bind hover and selection handlers
     forEach(ELEMENTS, id => {
       const element = getElement(id);
       element.addClass('element');
       bindElementInteractions(element);
     });
 
+    // (2) initialize modal
+    MicroModal.init();
+    forEach(ELEMENTS, id => {
+      const element = getElement(id);
+      bindOpenModal(element);
+    });
 
   });
 
@@ -58,8 +71,20 @@
     });
   }
 
+  function bindOpenModal(element) {
+    element.attr('data-micromodal-trigger', modalId);
+    element.on('dblclick', () => {
+      element.removeClass('hover');
+      element.addClass('selected');
+
+      MicroModal.show(modalId);
+    });
+  }
+
 </script>
 
 <div class="diagram">
   {@html DiagramSVG}
 </div>
+
+<CatalogModal {modalId} />
