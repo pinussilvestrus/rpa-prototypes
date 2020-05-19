956bda2b4b832b54f19a8122c7936cef3c6dda2d
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-05-19 08:02:05 +0200

    chore(element-template): rename package

---
+++ packages/element-template/src/components/PropertiesPanel.svelte
@@ -0,0 +1,164 @@
+<script>
+    import { afterUpdate } from 'svelte';
+
+    import { debounce, find } from 'min-dash';
+
+    import dom from 'domtastic';
+
+    import getElement from '../util/getElement';
+
+    import './PropertiesPanel.scss';
+
+
+    const TEMPLATES = [
+      {
+        id: 'get-price-tag',
+        type: 'UI Path Bot',
+        name: 'Get Price Tag',
+        topic: 'get-price-tag'
+      },
+      {
+        id: 'calculate-price',
+        type: 'UI Path Bot',
+        name: 'Calculate Price',
+        topic: 'calculate-price'
+      },
+      {
+        id: 'print-invoice',
+        type: 'UI Path Bot',
+        name: 'Print Invoice',
+        topic: 'print-invoice'
+      }
+    ];
+
+    const noop = () => {};
+
+    let isHidden = true;
+    $: isHidden = !element;
+
+    // populate
+    $: {
+      if (element) {
+        element.template = null;
+        if (element.templateId) {
+          element.template = getTemplate(element);
+        }
+      }
+    }
+
+    // lifecycle //////////
+
+    afterUpdate(async () => {
+      element && getElement(element.id).addClass('selected');
+    });
+
+
+    // methods //////////
+
+    const handlePropertyChanged = (event) => {
+      const inputNode = dom(event.target);
+
+      onPropertiesChanged(element.id, {
+        [inputNode.attr('name')]: inputNode.val()
+      });
+    };
+
+    const handleTemplateChanged = (event) => {
+      const selectNode = dom(event.target);
+
+      onPropertiesChanged(element.id, {
+        templateId: selectNode.val()
+      });
+    };
+
+    const handleSectionTitleClick = (event) => {
+      const titleNode = dom(event.target),
+            id = titleNode.attr('id'),
+            section = dom(`.section[data-title-ref="#${id}"]`);
+
+      if (section.hasClass('hidden')) {
+        titleNode.removeClass('inactive');
+        section.removeClass('hidden');
+      } else {
+        titleNode.addClass('inactive');
+        section.addClass('hidden');
+      }
+    };
+
+
+    // exports //////////
+
+    export let element = null;
+    export let onPropertiesChanged = noop;
+
+    
+    // helpers //////////
+
+    const getTemplate = (element) => {
+      return element && find(TEMPLATES, t => t.id === element.templateId);
+    };
+
+    // todo(pinussilvestrus): move sections to own class
+
+</script>
+
+<div class="properties-panel" class:hidden="{isHidden}">
+  <div class="container">
+    {#if element}
+      <div class="header">
+        <div class="title">{element.name}</div>
+      </div>
+
+      <div class="properties">
+        <div 
+          class="section-title" 
+          id="general" 
+          on:click={handleSectionTitleClick} >General <i>Icon</i></div>
+        <div class="section" data-title-ref="#general">
+  
+          <label>Id</label>
+          <input name="id" disabled value="{element.id}" />
+  
+          <label>Name</label>
+          <input name="name" on:input="{debounce(handlePropertyChanged, 500)}" value="{element.name}" />
+  
+          <label>Element Template</label>
+          <select id="element-template" on:change="{handleTemplateChanged}">
+            <option></option>
+            {#each TEMPLATES as {id, name}}
+              <option 
+                selected={element.template && element.template.id === id} 
+                value="{id}" >
+                  {name}
+              </option>
+            {/each}
+          </select>
+        </div>
+  
+        {#if element.template}
+          <div 
+            class="section-title" 
+            id="worker"
+            on:click={handleSectionTitleClick} >Worker details <i>Icon</i></div>
+          <div class="section" data-title-ref="#worker">
+  
+              <label>Type</label>
+              <input disabled value="{element.template.type}"/>
+  
+              <label>Name</label>
+              <input disabled value="{element.template.name}"/>
+  
+              <label>Topic</label>
+              <input disabled value="{element.template.topic}"/>
+  
+              <label>Input Parameters</label>
+              <textarea rows="5"></textarea>
+  
+              <label>Output Parameters</label>
+              <textarea rows="5"></textarea>
+          </div>
+        {/if}
+      </div>
+    {/if}
+  </div>
+</div>
