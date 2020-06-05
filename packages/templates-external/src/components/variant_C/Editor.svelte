e5f5f5ff4da29fcc4dade3a7bff94369edda4de2
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-06-05 20:05:04 +0200

    chore(templates-external): dummy variant c

---
+++ packages/templates-external/src/components/variant_C/Editor.svelte
@@ -0,0 +1,16 @@
+<script>
+    import BasicEditor from '../BasicEditor';
+
+    import ExpandableVariableList from '../variant_B/template/ExpandableVariableList';
+
+    import {
+      ELEMENTS as elements,
+      TEMPLATES as templates
+    } from '../../../resources/data';
+
+</script>
+
+<BasicEditor 
+  {elements} 
+  {templates} 
+  variableListComponent={ExpandableVariableList} />
