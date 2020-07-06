f22b25bfc7700881423c75af1276bdd9c55f438a
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-07-06 11:25:31 +0200

    feat(variable-management): introduce switch component

---
+++ packages/variable-management/src/components/Switch.svelte
@@ -0,0 +1,19 @@
+<script>
+    import './Switch.scss';
+    
+
+    // exports //////////
+
+    export let checked = false;
+    export let offLabel = 'off';
+    export let onLabel = 'on';
+</script>
+
+<div class="switch">
+    <label class="switcher">
+        <input type="checkbox" bind:checked />
+        <span class="slider" />
+    </label>
+      
+    <p class="label">{checked ? onLabel : offLabel}</p>
+</div>
